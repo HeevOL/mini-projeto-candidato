a89dcc33cc560f1371a409935fed5a36904472e8
adriantorress <adriantorrescv@gmail.com>
2022-10-09 11:59:55 -0300

    Implementação da fila (comum) + Listas Encadeadas

--- ListaNaoOrdenada.py
+++ ListaNaoOrdenada.py
@@ -1,41 +1,31 @@
 from Noh import *
 
+
 class ListaNaoOrdenada:
-    def __init__(self): #construtor
+    def __init__(self):
         self.head = None
         self.tamanho = 0
-        self.tail = Noh(self.head)
-    
+        # self.tail = Noh(self.head)
 
     def __str__(self): return f'{self.head}'
 
-
     def is_empty(self): return self.head == None
 
-
-    def add(self,item):
+    def add(self, item):
         temp = Noh(item)
         temp.setNext(self.head)
         self.head = temp
         self.tamanho += 1
-        if self.tamanho == 0:
-            self.tail.setData(self.head)
-        elif self.tamanho == 1:
-            self.tail.setData(item)
-
+        # if self.tamanho == 0:
+        #     self.tail.setData(self.head)
+        # elif self.tamanho == 1:
+        #     self.tail.setData(item)
 
     def size(self):
-        # atual = self.head
-        # contador = 0
-        # while atual != None:
-        #     contador = contador + 1
-        #     atual = atual.getNext()
-        # return contador
         return self.tamanho
 
-    
-    def search(self,item):
-        atual = self.head #atual == temp
+    def search(self, item):
+        atual = self.head
         encontrou = False
         while atual != None and not encontrou:
             if atual.getData() == item:
@@ -45,57 +35,66 @@ class ListaNaoOrdenada:
 
         return encontrou
 
-    def remove(self,item):
-        atual = self.head
-        anterior = None
-        encontrou = False
-        while not encontrou: #percorre a lista
-            if atual.getData() == item:
-                encontrou = True
+    def remove(self, item):
+        try:
+            atual = self.head
+            anterior = None
+            encontrou = False
+            while not encontrou:
+                if atual.getData() == item:
+                    encontrou = True
+                else:
+                    anterior = atual
+                    atual = atual.getNext()
+
+            if anterior == None:
+                self.head = atual.getNext()
             else:
-                anterior = atual
-                atual = atual.getNext()
+                anterior.setNext(atual.getNext())
+            self.tamanho -= 1
+        except:
+            print("Item não encontrado")
 
-        if anterior == None:
-            self.head = atual.getNext()
-        else:
-            anterior.setNext(atual.getNext())
-    
+    # def append(self, item):  # append O(n)
+    #     temp = Noh(item)
+    #     atual = self.head
+    #     count = 0
+    #     while count != self.tamanho-1:
+    #         atual = atual.getNext()
+    #         count += 1
 
-    def append(self, item): #append O(n)
-        temp = Noh(item)
-        atual = self.head
-        count = 0
-        while count != self.tamanho-1:
-            atual = atual.getNext()
-            count += 1
+    #     atual.setNext(temp)
+    #     self.tail = temp
+    #     self.tamanho += 1
 
-        atual.setNext(temp)
-        self.tail = temp
-        self.tamanho += 1
-
-    
-    def appendTest(self, item): # Tentativa de append O(1)
-        temp = Noh(item)
-        ultimo = Noh(self.tail.getData())
-        ultimo.setNext(temp)
-        self.tail = temp
+    # def appendTest(self, item):  # Tentativa de append O(1)
+    #     temp = Noh(item)
+    #     ultimo = Noh(self.tail.getData())
+    #     ultimo.setNext(temp)
+    #     self.tail = temp
 
 
 if __name__ == "__main__":
-
     lista = ListaNaoOrdenada()
-    print(lista.tail)
-    lista.add(30)
-    print(lista.tail)
+    print(lista.is_empty())
+    lista.add("Jose")
+    lista.add("Adrian")
+    lista.add("Torres")
+    lista.add("Dos")
+    lista.add("Santos")
     print(lista)
-    lista.add(60)
-    print(lista.tail)
+    print(lista.search("Adrian"))
+    lista.remove("Adrian")
     print(lista)
-    lista.add(90)
-    print(lista.tail)
-    print(lista)
-    lista.append(120)
+    print(lista.search("Adrian"))
+    lista.remove("Adrian")
     print(lista.size())
-    print(lista.tail)
-    print(lista)
+    lista.add("Adrian")
+    lista.add("Jose")
+    lista.add("Santos")
+    lista.add("Torres")
+    print(lista.size())
+    lista.remove("Adrian")
+    lista.remove("Torres")
+    lista.remove("Adrian")
+    print(lista.size())
